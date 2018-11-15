29421a2e42037e304bdcafae69e359240e1e48b4
charles loiseau <charles.loiseau@epitech.eu>
2018-11-15 14:17:02 +0100

    💯

--- src/Components/Room/Room.js
+++ src/Components/Room/Room.js
@@ -3,9 +3,26 @@ import { MessagesList } from "../../Containers/MessagesList"
 import { AddMessage } from "../../Containers/AddMessage"
 import React, { Component } from "react";
 import {Grid, Row, Col} from "react-bootstrap";
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import setupSocket from "../../Sockets"
+import reducers from "../../reducers"
+import handleNewMessage from '../../Sagas'
+import username from '../../Utils/name'
+
 import "./Room.css";
 
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const socket = setupSocket(store.dispatch, username)
+sagaMiddleware.run(handleNewMessage, {socket, username})
+
+
 export default class Room extends Component {
+    componentDidMount() {
+    }
+
     render() {
         return (
             <div>
@@ -13,18 +30,18 @@ export default class Room extends Component {
                     <Row className="show-grid">
                         <Col md={12} mdPull={12} className="show-grid">
                             <div>
-                                <Sidebar />
+                                <Sidebar store={store}/>
                             </div>
                         </Col>
                     </Row>
 
                     <Row className="show-grid">
                         <Col md={6} mdPull={6} className="show-grid">
-                            ma super animation
+                            
                         </Col>
                         <Col md={6} mdPull={6} className="show-grid">
-                            <MessagesList />
-                            <AddMessage />
+                            <MessagesList store={store}/>
+                            <AddMessage store={store} />
                         </Col>
                     </Row>
                 </Grid>
--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,14 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
-import { createStore } from 'redux'
-import chat from "./reducers"
 
 import './index.css';
 
-const store = createStore(chat)
+
 
 render(
   <Router>
-    <Provider store={store}>
+    <Provider>
         <App />
     </Provider>
     </Router>,
